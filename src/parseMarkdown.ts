36f6d6a7c7547cb7bc1c89b5bb5008a20848ab71
Niek Saarberg <n.saarberg@gmail.com>
2023-04-24 19:24:23 +0200

    Removed "previous" implementation code

--- src/parseMarkdown.ts
+++ src/parseMarkdown.ts
@@ -1,6 +1,5 @@
 import { MarkdownNode } from './nodes';
-import { ParserConfig } from './parser';
-import { mdAST } from './parser-v2';
+import { ParserConfig, mdAST } from './parser-v2';
 
 export function parseMarkdown(src: string, config?: ParserConfig): MarkdownNode[] {
   return mdAST().parse(src);
