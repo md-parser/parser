42ad33b02eebdcc4fa9df05edb6076cdf8846c6e
Niek Saarberg <n.saarberg@gmail.com>
2024-04-24 10:27:58 +0200

    Test test

--- src/v2/lexer/lexer.test.ts
+++ src/v2/lexer/lexer.test.ts
@@ -1,8 +1,28 @@
-import './lexer2';
+import { lexer } from './lexer2';
 
 describe('lexer', () => {
   it('should lex', () => {
-    const example = `###### ***heading***!`;
+    // const example = '###### ***heading***!';
+    const example = `# Heading 1
+## Heading 2
+### Heading 3
+
+> Bloc_kquote_
+
+\`\`\`js
+const a = 1;
+\`\`\`
+
+paragraph
+
+paragraph 2
+---
+`;
+    const tokens = lexer(example);
     // const getNextToken = lexer(example);
+
+    for (const token of tokens) {
+      console.log(token);
+    }
   });
 });
